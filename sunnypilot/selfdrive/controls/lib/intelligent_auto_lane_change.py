fa83a673fe425e88a72e482782711d40519f37c5
tsk-go <cmc686@gmail.com>
2025-11-18 13:33:56 -0500

    Update intelligent_auto_lane_change.py

--- sunnypilot/selfdrive/controls/lib/intelligent_auto_lane_change.py
+++ sunnypilot/selfdrive/controls/lib/intelligent_auto_lane_change.py
@@ -1,8 +1,8 @@
 from cereal import log
-from openpilot.common.params import Params
-from openpilot.common.realtime import DT_MDL
-from openpilot.common.numpy_fast import interp
-from openpilot.common.conversions import Conversions as CV
+from common.params import Params
+from common.realtime import DT_MDL
+from common.numpy_fast import interp
+from common.conversions import Conversions as CV
 
 # State machine for the intelligent auto lane change
 class IntelligentALCState:
